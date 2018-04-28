1e1da3740a92fca1f5dfaa9ba6b63becaaa7306a
Vipin Reyaroth (AU) <Vipin.VelluvaReyaroth@DimensionData.com>
2018-04-28 12:54:34 +1000

    Completed Day 3 challenge of Days7-9 (Data Structures)

---
+++ day7-9/day7to9challenge3.py
@@ -0,0 +1,23 @@
+from data import us_state_abbrev, states_list
+
+
+def main():
+    #Challenge 1: Print the 10th item in each data structure
+    #us_state_abbrev, which is a dictionary. So get the keys list and sort them
+    keys = sorted(us_state_abbrev.keys())
+
+    #To print 10th item, print k,v based on the key at index 9 on keys list
+    print("({}, {})".format(keys[9], us_state_abbrev[keys[9]]))
+
+    #states_list, which is a list. So print the one at index 9
+    print(states_list[9])
+
+    # Challenge 2: Print the 45th key in the dictionary. Just print the value at index 44 on keys list
+    print(keys[44])
+
+    # Challenge 3: Print the 27th value in the dictionary. Just print the key, where key is the value at index 26 on keys list
+    print(us_state_abbrev[keys[26]])
+
+
+if __name__ == '__main__':
+    main()
