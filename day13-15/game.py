47a8c7e10badd3de213412a4db3319860e5f5fde
vipinreyaroth <vipinreyaroth@gmail.com>
2018-05-04 00:01:49 +1000

    Completed the Rocks, Paper and Scissors role playing game code

--- day13-15/game.py
+++ day13-15/game.py
@@ -1,12 +1,18 @@
 
 class Roll:
-    def __init__(self, name, rolls_which_can_be_defeated, rolls_whcih_can_defeat_me):
+    def __init__(self, name):
         self.name = name
-        self.rolls_which_can_be_defeated = rolls_which_can_be_defeated
-        self.rolls_which_can_defeat_me = rolls_whcih_can_defeat_me
+        self.rolls_which_can_be_defeated = None
+        self.rolls_which_can_defeat_me = None
 
-    def can_defeat(selfs, Roll):
-        return True
+    def can_defeat(self, roll):
+        if self.rolls_which_can_be_defeated.name == roll.name:
+            return 'yes'
+        elif self.rolls_which_can_defeat_me.name == roll.name:
+            return 'no'
+        else:
+            return 'tie'
+
 
 class Person:
     def __init__(self, name):
--- day13-15/program.py
+++ day13-15/program.py
@@ -1,29 +1,96 @@
-from game import Roll, People
+from game import Roll, Person
 import random
 
 
 def print_header():
-    pass
+    print('-------------------------------------------------')
+    print('         Rock, Paper, Scissors!!!!               ')
+    print('-------------------------------------------------')
 
 
 def build_three_rolls():
-    return None
+    """
+    Building a list of three rolls (Rock, Paper and Scissor). Setting the properties too.
+    :return:
+    """
+    rock = Roll('Rock')
+    scissor = Roll('Scissor')
+    paper = Roll('Paper')
+
+    rock.rolls_which_can_be_defeated = scissor
+    rock.rolls_which_can_defeat_me = paper
+    scissor.rolls_which_can_be_defeated = paper
+    scissor.rolls_which_can_defeat_me = rock
+    paper.rolls_which_can_be_defeated = rock
+    paper.rolls_which_can_defeat_me = scissor
+
+    return [rock, scissor, paper]
 
 
 def get_players_name():
-    return None
+    """
+    Getting the name of the player. No error check is done.
+    :return:
+    """
+    name = input('Hello, what is your name? ')
+    return name
 
 
 def game_loop(player1, player2, rolls):
+    """
+    Game loop.
+    No error check is done for the user input
+    :param player1:
+    :param player2:
+    :param rolls:
+    :return:
+    """
     cnt = 1
+    p1_score = 0
+    p2_score = 0
 
-    while cnt < 3:
+    while cnt <= 3:
         p2_roll = random.choice(rolls)
-        p1_roll = rolls[0]
+        print(f'Round: {cnt}')
+        selection = input('Select your roll. Rock (r), Paper (p) or Scissors (s): ')
+
+        name = None
+        if selection == 'r':
+            name = 'Rock'
+        if selection == 'p':
+            name = 'Paper'
+        if selection == 's':
+            name = 'Scissors'
+
+        for r in rolls:
+            if r.name == name:
+                p1_roll = r
+                break
 
         outcome = p1_roll.can_defeat(p2_roll)
 
+        if outcome == 'yes':
+            winner = f'{player1.name}'
+            p1_score += 1
+        elif outcome == 'no':
+            winner = f'{player2.name}'
+            p2_score += 1
+        else:
+            winner = 'No one'
+            p1_score += 1
+            p2_score += 1
+
+        print(f'{player1.name} rolled {p1_roll.name}. {player2.name} rolled {p2_roll.name}.')
+        print(f'Winner of this round is: {winner}')
+
         cnt += 1
+
+    if p2_score > p1_score:
+        print(f'Winner of the game is {player2.name}')
+    elif p1_score > p2_score:
+        print(f'Winner of the game is {player1.name}')
+    else:
+        print('Everyone is a winner!!')
 
 
 def main():
@@ -32,8 +99,8 @@ def main():
 
     name = get_players_name()
 
-    player1 = Player(name)
-    player2 = Player('Computer')
+    player1 = Person(name)
+    player2 = Person('Computer')
 
     game_loop(player1, player2, rolls)
 
