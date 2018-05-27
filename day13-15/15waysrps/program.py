1a673e1b7a17377b05cccca3518896b4c2c50dea
vipinreyaroth <vipinreyaroth@gmail.com>
2018-05-27 22:52:22 +1000

    Day 13: Added logbook (python logger package)

--- day13-15/15waysrps/program.py
+++ day13-15/15waysrps/program.py
@@ -1,9 +1,13 @@
+import sys
+
+import logbook
 from game import Roll, Person
 import os
 import random
 import csv
 from collections import defaultdict
 
+app_logger = logbook.Logger('APP')
 
 def print_header():
     print('-------------------------------------------------------')
@@ -16,6 +20,7 @@ def get_player_name():
 
 
 def read_battle_data_from_file_and_create_rolls():
+    app_logger.trace('Reading battle data from file and creating Rolls (a list of Rolls)')
     rolls = defaultdict(Roll)
     rolls['Rock'] = Roll('Rock')
     rolls['Gun'] = Roll('Gun')
@@ -52,6 +57,7 @@ def read_battle_data_from_file_and_create_rolls():
 
 
 def list_rolls():
+    app_logger.trace('Listing the Rolls')
     rolls_with_index = dict()
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'battle-table.csv')) as f:
         header = next(f)
@@ -64,6 +70,7 @@ def list_rolls():
 
 
 def game_loop():
+    app_logger.trace('Game loop starting')
     player1 = Person(get_player_name())
     player2 = Person('Computer')
 
@@ -100,6 +107,8 @@ def game_loop():
 
         cnt += 1
 
+    app_logger.trace('Game loop ended.')
+    app_logger.trace('Finding the winner!')
     print('*************************************************')
     if p1_win == p2_win:
         print('Everyone is a winner!!')
@@ -109,10 +118,25 @@ def game_loop():
         print(f'{player2.name} is the final Winner!!!')
     print('*************************************************')
 
+
+def init_logger(filename: str = None):
+    level = logbook.TRACE
+
+    if filename:
+        logbook.TimedRotatingFileHandler(filename, level=level).push_application()
+    else:
+        logbook.StreamHandler(sys.stdout, level=level).push_application()
+
+    startup_logger = logbook.Logger('Start Up')
+    msg = 'Logging initialised. mode = {} level = {}'.format("file mode" if filename else "stdout mode", level)
+    startup_logger.notice(msg)
+
+
 def main():
     print_header()
     game_loop()
 
 
 if __name__ == '__main__':
+    init_logger('15wayrps-game')
     main()
