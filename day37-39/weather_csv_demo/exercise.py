1427215c83f7bb39bb2f9d28c8b3b3d6e7fc229d
vipinreyaroth <38558860+vipinreyaroth@users.noreply.github.com>
2018-06-17 23:19:19 +1000

    Added profiling

--- day37-39/weather_csv_demo/exercise.py
+++ day37-39/weather_csv_demo/exercise.py
@@ -1,30 +1,38 @@
 import research
+import cProfile
+profiler = cProfile.Profile()
+profiler.disable()
 
 
 def main():
+    profiler.enable()
     # init
     research.init()
+    hot_days = research.get_hot_days()
+    cold_days = research.get_cold_days()
+    wet_days = research.get_wettest_days()
+    profiler.disable()
 
     # print the 5 hottest days
     print('5 Hottest days')
-    hot_days = research.get_hot_days()
     for day in hot_days[:5]:
         print(f'{day.actual_max_temp}F on {day.date}')
 
     # print the 5 coolest dates
     print()
     print('5 Coldest days')
-    cold_days = research.get_cold_days()
     for day in cold_days[:5]:
         print(f'{day.actual_min_temp}F on {day.date}')
 
     # print the wettest dates
     print()
     print('5 Wettest days')
-    wet_days = research.get_wettest_days()
     for day in wet_days[:5]:
         print(f'{day.actual_precipitation} inches on {day.date}')
 
 
 if __name__ == '__main__':
-    main()
+    for _ in range(1, 100):
+        main()
+
+    profiler.print_stats(sort='cumtime')
--- day37-39/weather_csv_demo/research.py
+++ day37-39/weather_csv_demo/research.py
@@ -5,21 +5,19 @@ import pprint
 from typing import List
 
 data = []
-Record = collections.namedtuple('Record', 'date,actual_mean_temp,actual_min_temp,'
-                                          'actual_max_temp,average_min_temp,average_max_temp,'
-                                          'record_min_temp,record_max_temp,record_min_temp_year,record_max_temp_year,'
-                                          'actual_precipitation,average_precipitation,record_precipitation')
+Record = collections.namedtuple('Record', 'date, actual_min_temp, actual_max_temp, actual_precipitation')
 
 
 def init():
     filename = os.path.join(os.path.dirname(__file__), 'data', 'seattle.csv')
 
-    with open(filename) as fin:
-        reader = csv.DictReader(fin)
+    if not data:
+        with open(filename) as fin:
+            reader = csv.DictReader(fin)
 
-        data.clear()
-        for row in reader:
-            data.append(parse(row))
+            data.clear()
+            for row in reader:
+                data.append(parse(row))
 
 
 def get_hot_days() -> List[Record]:
@@ -35,19 +33,14 @@ def get_wettest_days() -> List[Record]:
 
 
 def parse(row):
-    row['actual_mean_temp'] = int(row['actual_mean_temp'])
     row['actual_min_temp'] = int(row['actual_min_temp'])
     row['actual_max_temp'] = int(row['actual_max_temp'])
-    row['average_min_temp'] = int(row['average_min_temp'])
-    row['average_min_temp'] = int(row['average_min_temp'])
-    row['record_min_temp'] = int(row['record_min_temp'])
-    row['record_max_temp'] = int(row['record_max_temp'])
-    row['record_max_temp'] = int(row['record_max_temp'])
-    row['record_min_temp_year'] = int(row['record_min_temp_year'])
-    row['record_max_temp_year'] = int(row['record_max_temp_year'])
     row['actual_precipitation'] = float(row['actual_precipitation'])
-    row['average_precipitation'] = float(row['average_precipitation'])
-    row['record_precipitation'] = float(row['record_precipitation'])
 
-    record = Record(**row)
+    record = Record(
+                date=row.get('date'),
+                actual_min_temp=row.get('actual_min_temp'),
+                actual_max_temp=row.get('actual_max_temp'),
+                actual_precipitation=row.get('actual_precipitation')
+                )
     return record
