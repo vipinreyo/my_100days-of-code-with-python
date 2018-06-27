0b29e170b93d860d94e7d1260ee05dfb742686da
vipinreyaroth <38558860+vipinreyaroth@users.noreply.github.com>
2018-06-28 09:38:56 +1000

    Completed the 100dayscode tweet automation

--- days58-61/day2_challenge/100daysofcode_autotweet.py
+++ days58-61/day2_challenge/100daysofcode_autotweet.py
@@ -9,7 +9,6 @@ PROGRESS_LOG = 'https://raw.githubusercontent.com/vipinreyo/my_100days-of-code-w
 def get_progress_log():
     response = requests.get(PROGRESS_LOG)
     response.raise_for_status()
-    #print(response.text)
     return response
 
 
@@ -20,18 +19,24 @@ def get_day_progress(log):
 
 
 def create_tweet(title):
-    tweet_format = "Completed '{}' https://talkpython.fm/100days?s=pybites #Python @pybites @talkpython".format(title)
+    tweet_format = "Completed '{}' https://talkpython.fm/100days?s=pybites #Python @pybites " \
+                   "@talkpython #automated".format(title)
     return tweet_format
 
 
-def tweet_progress():
-    pass
+def tweet_progress(tweet):
+    try:
+        api.update_status(tweet)
+        print('Posted to twitter account')
+    except Exception as ex:
+        print('Unable to post to twitter. {}'.format(ex))
 
 
 def main():
     resp = get_progress_log()
     title = get_day_progress(resp.text)
-    print(create_tweet(title))
+    tweet = create_tweet(title)
+    tweet_progress(tweet)
 
 
 if __name__ == '__main__':
