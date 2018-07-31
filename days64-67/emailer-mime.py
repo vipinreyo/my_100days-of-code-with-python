de950cbcbb1036caf3dfd88f722521b8c4948c85
vipinreyaroth <vipinreyaroth@gmail.com>
2018-08-01 00:48:54 +1000

    Script which uses MIME types

--- days64-67/emailer-mime.py
+++ days64-67/emailer-mime.py
@@ -1,2 +1,32 @@
-from dotenv import load_dotenv
-load_dotenv()
+import os, smtplib
+
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+GOOGLE_APP_PASSCODE = os.getenv("GOOGLE_APP_PASSCODE")
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from_email = "vipinreyo.test@gmail.com"
+to_email = "vipinreyo.test@gmail.com"
+
+msg = MIMEMultipart()
+msg['From'] = from_email
+msg['To'] = to_email
+msg['Subject'] = 'This is a mail from the SMTP mail python script'
+
+body = """ Hello, this is a friendly message from the SMTP mail python script. You Rock!!!"""
+
+msg.attach(MIMEText(body, 'plain'))
+
+smtp_server = smtplib.SMTP('smtp.gmail.com', 587)
+smtp_server.ehlo()
+smtp_server.starttls()
+smtp_server.login('vipinreyo.test@gmail.com', GOOGLE_APP_PASSCODE)
+
+text = msg.as_string()
+smtp_server.sendmail(from_email, to_email, text)
+smtp_server.quit()
+
+print('Email sent successfully...')
